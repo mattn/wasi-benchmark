06e59d7bb7acc176d7c9da3b6afcb1b8d95923d8
Hiroki Noda <kubo39@gmail.com>
2024-04-10 21:17:33 +0900

    Tweaks
    Signed-off-by: Hiroki Noda <kubo39@gmail.com>

--- main.d
+++ main.d
@@ -2,11 +2,12 @@ extern (C):
 nothrow:
 @nogc:
 
+enum EX_SOFTWARE = 70;
+
 __gshared int errno;
 void _Exit(int status) @trusted;
 void perror(const char* s) @trusted;
 pragma(printf) int printf(const(char)* format, ...) @trusted;
-size_t strlen(const char* s) @trusted;
 uint strtoul(const char* str, char** str_end, int base) @trusted;
 
 uint fib(uint n)
@@ -21,15 +22,14 @@ int __main_argc_argv(int argc, char** argv)
     if (argc != 2)
     {
         perror("error: argument not given");
-        _Exit(70);
+        _Exit(EX_SOFTWARE);
     }
-    char[] s = argv[1][0 .. strlen(argv[1])];
     char* end = null;
-    uint n = strtoul(s.ptr, &end, 10);
+    uint n = strtoul(argv[1], &end, 10);
     if (errno != 0)
     {
         perror("error: argument not a integer");
-        _Exit(70);
+        _Exit(EX_SOFTWARE);
     }
     printf("%lu\n", fib(n));
     return 0;
